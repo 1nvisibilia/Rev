68cc365a7e909f170c128772bf220caa87e1b0d5
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2024-08-17 16:39:23 -0400

    init

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	envLoadErr := godotenv.Load()
+	if envLoadErr != nil {
+		log.Fatal("Cannot load .env file")
+	}
+	backend_server_port := os.Getenv("BACKEND_SERVER_PORT")
+	proxy_starting_port := os.Getenv("PROXY_PORT")
+
+	target, err := url.Parse(backend_server_port)
+
+	if err != nil {
+		log.Fatal("Cannot parse URL:", err)
+	}
+
+	proxyServer := httputil.NewSingleHostReverseProxy(target)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		r.Host = target.Host
+		proxyServer.ServeHTTP(w, r)
+	})
+
+	log.Println("Starting server")
+	log.Println("		Proxy Server		==>>		Actual Server")
+	log.Println("		" + proxy_starting_port + "		==>>		" + backend_server_port)
+	log.Fatal(http.ListenAndServe(proxy_starting_port, nil))
+}
